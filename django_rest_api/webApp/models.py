2022ac3e8445880bb8110fc15613553f4f6aab5d
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-03-10 21:32:39 +0530

    Initial Commit

---
+++ django_rest_api/webApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class employees(models.Model):
+    firstName = models.CharField(max_length=10)
+    lastName = models.CharField(max_length=10)
+    emp_id = models.IntegerField()
+
+    def __str__(self):
+        return self.firstName
