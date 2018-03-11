85e1a77bdaaac07bf153332133910ac526f65213
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-03-11 16:10:59 +0530

    Initial Commit

--- django_rest_api/django_rest_api/urls.py
+++ django_rest_api/django_rest_api/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from webApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employees/', views.EmployeeList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- django_rest_api/webApp/views.py
+++ django_rest_api/webApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import employees
+from .serializer import employeesSerializer
 
-# Create your views here.
+# Lists all employees or create a new one
+#employees/
+
+class EmployeeList(APIView):
+
+    def get(self,request):
+        employee = employees.objects.all()
+        serializer = employeesSerializer(employee, many=True)
+        return Response(serializer.data)
+
+
